d40ac1cc3ab31289bc1aef59791dcd18864020e6
TimLit13 <litidm@gmail.com>
2021-08-29 20:09:37 +0300

    Add seeds for db with comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 50.times do
   title = Faker::Hipster.sentence(word_count: 3)
   body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
-  Question.create title: title, body: body
+  @q = Question.create title: title, body: body
+  50.times do |i|
+    body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
+    Answer.create question_id: @q.id, body: body 
+  end
 end
