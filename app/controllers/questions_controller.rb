0d2795dfd2966873e03c1b657991b1a94c808e79
TimLit13 <litidm@gmail.com>
2021-08-29 10:45:43 +0300

    Add actions and views for edit and update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -20,6 +20,15 @@ class QuestionsController < ApplicationController
     @question = Question.find_by id: params[:id]
   end
 
+  def update
+    @question = Question.find_by id: params[:id]
+    if @question.update question_params
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
   def question_params
     params.require(:question).permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :questions, only: %i[index new create edit]
+  resources :questions, only: %i[index new create edit update]
 
   root 'pages#index'
 end
